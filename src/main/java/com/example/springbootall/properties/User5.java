f054d1336103e92d56d94693689e5ed37a5f3a5d
liangzhixin <zhixin.liang@jieshunpay.cn>
2018-10-17 14:12:30 +0800

    modify

--- src/main/java/com/example/springbootall/properties/User5.java
+++ src/main/java/com/example/springbootall/properties/User5.java
@@ -6,7 +6,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
- * Environment(所有已经被加载的配置都可以读取到)
+ * Environment:
+ *  1.读取默认配置文件的属性
+ *  2.如果需要读取自定义配置文件,需要加载该配置文件才能读取(通过@PropertySource加载)
  */
 @Component
 public class User5 {
@@ -14,12 +16,14 @@ public class User5 {
     @Autowired
     private Environment environment;
 
-//    @Scheduled(cron = "0/3 * * * * ?")
+    @Scheduled(cron = "0/3 * * * * ?")
     public void test(){
+        //读取默认配置文件的属性
         String name = environment.getProperty("lzx.name");
         String age = environment.getProperty("lzx.age");
         System.out.println(name + ":" + age);
 
+        //读取自定义配置文件的属性(需要先加载才能读取)
         String name2 = environment.getProperty("test.name");
         String age2 = environment.getProperty("test.age");
         System.out.println(name2 + ":" + age2);

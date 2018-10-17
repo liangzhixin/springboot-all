d91bc85207584e4afedd036a0f9f4decacafd831
liangzhixin <zhixin.liang@jieshunpay.cn>
2018-10-17 16:17:34 +0800

    modify

--- README.md
+++ README.md
@@ -6,6 +6,15 @@
  
  
  
+/**
+ *  配置Druid连接池+多数据源+Mybatis
+ *  1.只整合Mybatis时,我们只需要在配置文件配置相关属性,springboot会自动生成默认的dataSource,sqlSessionFactory,transactionManager
+ *  2.配置Druid连接池时,需要我们自己去配置dataSource,sqlSessionFactory,transactionManager
+ *  3.配置多数据源时,也需要我们自己去配置
+ */
+ 
+ 
+ 
   数据库连接池的作用详见https://blog.csdn.net/u011225629/article/details/48317919
   
   
--- src/main/java/com/example/springbootall/SpringbootAllApplication.java
+++ src/main/java/com/example/springbootall/SpringbootAllApplication.java
@@ -2,6 +2,7 @@ package com.example.springbootall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -9,6 +10,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SpringbootAllApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootAllApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(SpringbootAllApplication.class, args);
+//        String[] beanNames = context.getBeanDefinitionNames();
+//        for (String beanName : beanNames){
+//            System.out.println(beanName);
+//        }
     }
 }
